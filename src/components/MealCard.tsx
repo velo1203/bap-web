dcb1ddbe1377130c0142e878e61ff2c09050eee6
velo1203 <hokitoki1203@gmail.com>
2025-03-10 16:32:07 +0900

    feat/can copy with click

--- src/components/MealCard.tsx
+++ src/components/MealCard.tsx
@@ -9,21 +9,29 @@ type MealCardProps = {
 const MealCard = ({ type, menu, loading }: MealCardProps) => {
     const mealTitle =
         type === "breakfast" ? "조식" : type === "lunch" ? "중식" : "석식";
+
+    const mealText = `<오늘의 ${mealTitle}>\n\n${menu.replace(
+        /\//g,
+        "\n"
+    )}\n\n급식 정보는? 밥.net`;
     const handleShare = async () => {
         if (navigator.share) {
             try {
                 await navigator.share({
                     title: `${mealTitle} 메뉴`,
-                    text: `<오늘의 ${mealTitle}>\n\n${menu.replace(
-                        /\//g,
-                        "\n"
-                    )}\n\n급식 정보는? 밥.net`,
+                    text: mealText,
                 });
             } catch (error) {
                 console.error("공유 실패:", error);
             }
         } else {
-            alert("이 브라우저에서는 공유 기능을 지원하지 않습니다.");
+            try {
+                await navigator.clipboard.writeText(mealText);
+                alert("급식 정보가 클립보드에 복사되었습니다! 🎉");
+            } catch (error) {
+                console.error("클립보드 복사 실패:", error);
+                alert("클립보드 복사에 실패했습니다.");
+            }
         }
     };
     return (
